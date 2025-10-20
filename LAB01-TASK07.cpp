427a2e43e2bbe599ac84004c1089623e75cb2a7a
aneeezashafqtt <aneeeza.shafqat@gmail.com>
2025-10-20 20:39:43 +0400

    Add files via upload

---
+++ LAB01-TASK07.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<< " Hey Students.Solve me and get your deserved Marks ^_^";
+		cout<< " Good Luck!";
+	cout<< " I hope you all are working really hard to achieve best of the results!";
+		return 0;
+}
